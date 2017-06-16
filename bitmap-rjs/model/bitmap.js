ac9634a5fed225839d4178b40aa91ffa26c82830
0smium <steve.walsh@transfueler.com>
2017-06-16 16:27:52 -0700

    added error handling and formated to be called from index.js

--- bitmap-rjs/model/bitmap.js
+++ bitmap-rjs/model/bitmap.js
@@ -2,11 +2,29 @@
 
 const fs = require('fs');
 
-module.exports = () => {
-  fs.readFile('../assets/bitmap.bmp', (err, data) => {
+module.exports = (path, xform, callback) => {
+  fs.readFile(path, (err, data) => {
 
     if (err)
-      console.error(err);
+      return callback(err);
+
+    if (data.slice(0,2).toString() != 'BM')
+      return callback(new Error('This app only handles Windows bitmap format: \'BM\'.'));
+    if (data.length < 1081)
+      return callback(new Error('This file is too short to be a bitmap.'));
+    if (data.readUInt16LE(10) != 1078)
+      return callback(new Error('This app only handles Windows bitmap format.  Expected to see pixel array offset at byte 1078.'));
+
+    function MetaData (data) {
+      this.headerField = data.slice(0,2).toString(); // 02
+      this.size = data.readUInt16LE(2); //02-06
+      this.width = data.readUInt16LE(18);// 18-21
+      this.height = data.readUInt16LE(22);// 22-25
+      this.bitDepth = data.readUInt16LE(28);// 28-29
+    }
+
+    let fileData = new MetaData(data);
+    console.log(fileData);
 
     let arrayOffset = data.readUInt16LE(10);
 
@@ -33,6 +51,7 @@ module.exports = () => {
         i = i + 4;
       }
     };
+    xform();
 
     //Push colorTableArr back into colorTableBuf
     let newColorTableBuf = Buffer.from(colorTableArr);
@@ -40,18 +59,9 @@ module.exports = () => {
     //Recombine all four buffers
     let newImgBuf = Buffer.concat([headerBuf, DIBBuf, newColorTableBuf, pixMapBuf], data.length);
 
-    fs.writeFile('../assets/bitmap-new.bmp', newImgBuf, (err) => {if (err) {console.error(err);} });
+    fs.writeFile(path + '.new.bmp', newImgBuf, (err) => {if (err) {return callback(err);} });
 
-    function Headers (data) {
-      this.headerField = data.slice(0,2).toString(); // 02
-      this.size = data.readUInt16LE(2); //02-06
-      this.width = data.readUInt16LE(18);// 18-21
-      this.height = data.readUInt16LE(22);// 22-25
-      this.bitDepth = data.readUInt16LE(28);// 28-29
-    }
-
-    let newImgHeader = new Headers(headerBuf);
-    console.log(newImgHeader);
+    return callback('done');
 
   });
 };
